400f38a7d01b4bfdc9e0b06b47e5555622d9c126
UsaidAl <ali_usaid98@hotmail.com>
2024-03-07 21:01:55 +0000

    updates

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
-const uuid = require('uuid');
-
+const api = require("./routes/index")
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3001;
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,12 +11,16 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+)
+
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,"/public/index.html" ));
+    res.sendFile(path.join(__dirname,"./public/index.html" ));
   });
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 
