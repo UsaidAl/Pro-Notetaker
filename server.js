2af345a02a510ca155ed350467dd077bd557d9a8
UsaidAl <ali_usaid98@hotmail.com>
2024-03-07 19:50:31 +0000

    Server started

--- server.js
+++ server.js
@@ -45,3 +45,8 @@ app.get('/notes', (req, res) => {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
+
+
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
