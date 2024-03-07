a6bba0604a092939004d60bf5dd6d7960ba01615
UsaidAl <ali_usaid98@hotmail.com>
2024-03-07 19:48:20 +0000

    HTML routes created to serve HTML files

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+  res.json(notes);
+});
+
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+  newNote.id = uuid.v4(); // Generate a unique ID
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes));
+  res.json(newNote);
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+  const updatedNotes = notes.filter(note => note.id !== noteId);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(updatedNotes));
+  res.json({ message: 'Note deleted' });
+});
+
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
