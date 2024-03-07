4e5784e7cfbd3b9928ae9e8d8e699d23fe534af0
UsaidAl <ali_usaid98@hotmail.com>
2024-03-07 22:01:12 +0000

    Updates

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-const express = require('express');
-const path = require('path');
-const api = require("./routes/index")
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index");
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+PORT = process.env.PORT || 3001;
 
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-)
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,"./public/index.html" ));
-  });
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-
-
-  app.listen(PORT, () => {
-    console.log(`The server is now listening to http://localhost:${PORT}`);
-  });
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
